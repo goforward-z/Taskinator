14016218c288acae7b9c9e86550cea31697b14bb
goforward-z <zlepeng.ucf@gmail.com>
2022-05-03 14:55:27 -0400

    updated form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
-var formE1 = document.querySelector("#task-form");
-var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler=function(event){
-    
-    event.prevenDefault();
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+//create list item
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  //create div to hold task info and add to list item
+  var taskInfoE1 = document.createComment("div");
+  //give it a class name
+  taskInfoE1.className ="task-info";
+  //add HTML content to div
+  taskInfoE1.innerHTML ="<h3 class='task-name'>" + taskNameInput +"</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoE1);
 
-    var listItemE1 = document.createElement("li");
-    listItemE1.className="task-item";
-    listItemE1.textContent="This is a new task.";
-    tasksToDoE1.appendChild(listItemE1);
-};
+  //add entire list item to list
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
 
-
-formE1.addEventListener("submit",createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
